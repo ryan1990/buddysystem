28f2186ec9c488ed488863821a4d2c5183a88f01
Ryan Mays <ryanmays1990@gmail.com>
2019-09-26 15:06:53 -0700

    got email validation working

--- components/LoginScreen.js
+++ components/LoginScreen.js
@@ -1,32 +1,51 @@
 import * as React from 'react';
 import { Alert, Button, Text, TextInput, View, StyleSheet } from 'react-native';
+//import * as EmailValidator from 'email-validator';
+// should remove email-validator from file system if not used!
+import { validateEmailAddress } from './Validator';
 
 export default class LoginScreen extends React.Component {
+  
   login() {
+    let emailText = "dummyEmailText@aol.net";
+    if (!validateEmail(emailText)) {
+      Alert.alert("Please enter a valid email address.");
+      return;
+    } else {
+      // TEMP: remove!
+      Alert.alert("GOOD EMAIL!!!");      
+    }
+
     let userExists = true; // will make api call to backend
 
     if (userExists) {
       this.props.goToStopwatchScreen(); // show user logged in
-      // do email validation!
+
     } else {
       Alert.alert("There is no account associated with this email address. Check your spelling or click Create new account.");
       // how can user update their goal/commitment later?
       // separate account creation for timing from create/update of commitment/goal?
       // have new screen that logged in user can use to update commitment/goal!
     }
+
+    function validateEmail(input) {
+      //return EmailValidator.validate(input);
+      return validateEmailAddress(input);
+    }
   }
   
   render() {
     return (
       <View>
-        <Text>
-          LoginScreen!
-        </Text>
-        <Text>Enter your email address:</Text>
+        <Text>Login with your email address if you have an account:</Text>
         <TextInput
-          style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
-          value={"Email address"}
+          style={{height: 40, borderColor: 'gray', borderWidth: 1 }}
+          placeholder="Email address"
+          // onChangeText={(text) => this.setState({text})}
+          // value={this.state.text}
         />
+
+
         <Text />
         <Button title="Login" onPress={this.login.bind(this)} />
         <Text />
